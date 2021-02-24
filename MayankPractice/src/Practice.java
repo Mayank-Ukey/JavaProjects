7c9c5ec3b37452fbfca152cb9da6312b451c1319
Mayank_Ukey <mayank_ukey@persistent.com>
2021-02-24 14:28:28 +0530

    Abstract Methods and Abstract Class in Java

--- MayankPractice/src/Practice.java
+++ MayankPractice/src/Practice.java
@@ -1,120 +1,39 @@
-import java.util.Scanner;
+// Abstract Class and Method in Java
 
-class Library{
-    String[] availableBooks;
-    String[] issuedBooks;
-    static int i=0;
-    static int j=0;
-    Library(int numBooks){
-        availableBooks = new String[numBooks];
-        issuedBooks = new String[numBooks];
+
+// NOTE: Abstract is such a class, with the help of which other classes can be created. (Abstract sets a standard for
+// implementing methods.)
+abstract class Parent{
+    public Parent(){
+        System.out.println("I am parent class.");
     }
+    abstract public void greet();
+}
 
-    public void addBook(String book){
-        if(i==availableBooks.length){
-            System.out.println("Can't add more books.");
-        }
-        else{
-            availableBooks[i] = book;
-            i++;
-        }
-    }
-
-    public int issueBook(){
-        Scanner sc  = new Scanner(System.in);
-        if(j==issuedBooks.length-1){
-            System.out.println("You can't issue more books.");
-        }
-        else{
-            String name;
-            System.out.println("Enter the name of book you wanna issue: ");
-            name = sc.next();
-            for (String val:availableBooks){
-                int k=0;
-                if(name.equals(val)){
-                    for (int x=k; x<availableBooks.length-1; x++){
-                        availableBooks[x] = availableBooks[x+1];
-                    }
-                    i--;
-                    issuedBooks[j] = name;
-                    j++;
-                    return 1;
-                }
-                else{
-                    k++;
-                }
-            }
-        }
-        return 0;
-    }
-
-    public void returnBook(){
-        Scanner sc = new Scanner(System.in);
-        String name;
-        System.out.println("Enter the name of the book, you wanna return: ");
-        name = sc.next();
-        availableBooks[i] = name;
-        i++;
-    }
-
-    public void showAvailableBooks(){
-        int m=0;
-        while(m<i){
-            System.out.println(availableBooks[m]+" ");
-            m++;
-        }
+class Child extends Parent{
+    @Override
+    public void greet(){
+        System.out.println("Hello, I'm a child class and have a good day.");
     }
 }
 
-public class Practice {
+class Child2 extends Parent{
+    @Override
+    public void greet(){
+        System.out.println("I am the second child of parent class.");
+    }
+}
+
+public class Practice{
     public static void main(String[] args) {
-        // Implement a library using Java class library
-        // Methods: addBook, issueBook, returnBook, showAvailableBooks
-        // Properties: Array to store the available books
-        // and an array to store the issue books.
-        Scanner sc = new Scanner(System.in);
-        int numBooks;
-        System.out.println("Enter the number of books, you want to make available.");
-        numBooks = sc.nextInt();
+        Parent p = new Child(); // We can give reference of the abstract class and can create object of the child class.
+        p.greet();
+//        Parent p = new Parent(); // We can't create objects of abstract classes.
+        // But we can use the constructor of parent class.
+        Child c = new Child();
+        c.greet();
 
-        Library l = new Library(numBooks);
-        int choice;
-        do {
-            System.out.println("Enter your choice: ");
-            System.out.println("1. Add Book: ");
-            System.out.println("2. Issue Book: ");
-            System.out.println("3. Return Book: ");
-            System.out.println("4. Show Available Books: ");
-            System.out.println("5. Exit");
-            choice = sc.nextInt();
-
-            switch(choice){
-                case 1:
-                    String name;
-                    System.out.println("Enter the book name you want to add: ");
-                    name = sc.next();
-                    l.addBook(name);
-                    System.out.println("Book Added.");
-                    break;
-
-                case 2:
-                    int res = l.issueBook();
-                    if(res==1){
-                        System.out.println("Book issued.");
-                    }
-                    else{
-                        System.out.println("Book is not issued.");
-                    }
-                    break;
-
-                case 3:
-                    l.returnBook();
-                    break;
-
-                case 4:
-                    l.showAvailableBooks();
-            }
-        }while(choice<5);
-        System.out.println("You are out of the program.");
+        Child2 c2 = new Child2();
+        c2.greet();
     }
 }
